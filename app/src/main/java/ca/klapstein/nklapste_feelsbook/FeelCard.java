7ac5d025ee178e14c2da2f662a847c6dd205b5ce
nklapste <nklapste@ualberta.ca>
2018-09-09 13:28:22 -0600

    initial commit

---
+++ app/src/main/java/ca/klapstein/nklapste_feelsbook/FeelCard.java
@@ -0,0 +1,9 @@
+package ca.klapstein.nklapste_feelsbook;
+
+public class FeelCard {
+    private static final String TAG = "FeelCard";
+
+    public FeelCard() {
+
+    }
+}
---
+++ app/src/main/java/ca/klapstein/nklapste_feelsbook/MainActivity.java
@@ -0,0 +1,14 @@
+package ca.klapstein.nklapste_feelsbook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
