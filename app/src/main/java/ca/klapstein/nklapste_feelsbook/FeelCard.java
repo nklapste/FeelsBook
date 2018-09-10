899f21e4f124c0eb71fca598ecb5d4982055c0b5
nklapste <nklapste@ualberta.ca>
2018-09-10 11:55:51 -0600

    adding `FeelCard` object class

--- app/src/main/java/ca/klapstein/nklapste_feelsbook/FeelCard.java
+++ app/src/main/java/ca/klapstein/nklapste_feelsbook/FeelCard.java
@@ -3,7 +3,23 @@ package ca.klapstein.nklapste_feelsbook;
 public class FeelCard {
     private static final String TAG = "FeelCard";
 
-    public FeelCard() {
+    private String comment;
+    private Feel feel;
 
+    FeelCard(Feel feel) {
+        this.feel = feel;
+        this.comment = "";
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Feel getFeel() {
+        return feel;
     }
 }
