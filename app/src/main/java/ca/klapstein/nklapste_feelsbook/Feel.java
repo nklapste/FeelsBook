c9720c5efd0b07862e13345609f77b925db5df95
nklapste <nklapste@ualberta.ca>
2018-09-11 09:19:35 -0600

    adding comment

--- app/src/main/java/ca/klapstein/nklapste_feelsbook/Feel.java
+++ app/src/main/java/ca/klapstein/nklapste_feelsbook/Feel.java
@@ -4,6 +4,15 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+/**
+ * Object Class defining a feeling as described by:
+ *
+ * http://changingminds.org/explanations/emotions/basic%20emotions.htm from
+ * Shaver, P., Schwartz, J., Kirson, D., & O'Connor, C. (2001).
+ * Emotional Knowledge: Further Exploration of a Prototype Approach.
+ * In G. Parrott (Eds.), Emotions in Social Psychology: Essential Readings (pp. 26-56).
+ * Philadelphia, PA: Psychology Press.
+ */
 public class Feel {
     private static final String TAG = "Feel";
 
@@ -17,6 +26,7 @@ public class Feel {
     private String comment;
     private String feeling;
     private String date;
+
     public static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss", Locale.getDefault());
 
     Feel(String feeling) {
