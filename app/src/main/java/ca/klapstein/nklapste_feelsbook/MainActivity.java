dcf730783764228284122712510e4e25f0c09d7a
nklapste <nklapste@ualberta.ca>
2018-09-11 10:08:13 -0600

    refactoring

--- app/src/main/java/ca/klapstein/nklapste_feelsbook/MainActivity.java
+++ app/src/main/java/ca/klapstein/nklapste_feelsbook/MainActivity.java
@@ -247,7 +247,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
 
             default:
-                Log.e(TAG, "Unsupported button pushed: "+ view.getId());
+                Log.e(TAG, "Unsupported button pushed: " + view.getId());
                 return;
         }
         mFeelList.add(0, new Feel(feeling));
