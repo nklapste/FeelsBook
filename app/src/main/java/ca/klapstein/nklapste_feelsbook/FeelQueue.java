05cb12779a683759443e1561eb03ccede3a70d18
nklapste <nklapste@ualberta.ca>
2018-09-13 08:37:46 -0600

    overriding `offer` instead of `add`

--- app/src/main/java/ca/klapstein/nklapste_feelsbook/FeelQueue.java
+++ app/src/main/java/ca/klapstein/nklapste_feelsbook/FeelQueue.java
@@ -4,6 +4,11 @@ import android.util.Log;
 
 import java.util.PriorityQueue;
 
+/**
+ * A PriorityQueue subclass that only accepts {@code Feel}s.
+ *
+ * Additionally running tallies of each feeling are kept for quick statistics generation.
+ */
 public class FeelQueue extends PriorityQueue<Feel> {
     private static final String TAG = "FeelQueue";
 
@@ -65,38 +70,41 @@ public class FeelQueue extends PriorityQueue<Feel> {
     }
 
     @Override
-    public boolean add(Feel feel) {
-        String feeling = feel.getFeeling();
-        switch (feeling) {
-            case Feel.ANGER:
-                angerTally += 1;
-                break;
+    public boolean offer(Feel feel) {
+        boolean offerResult = super.offer(feel);
+        if (offerResult) {
+            String feeling = feel.getFeeling();
+            switch (feeling) {
+                case Feel.ANGER:
+                    angerTally += 1;
+                    break;
 
-            case Feel.FEAR:
-                fearTally += 1;
-                break;
+                case Feel.FEAR:
+                    fearTally += 1;
+                    break;
 
-            case Feel.JOY:
-                joyTally += 1;
-                break;
+                case Feel.JOY:
+                    joyTally += 1;
+                    break;
 
-            case Feel.LOVE:
-                loveTally += 1;
-                break;
+                case Feel.LOVE:
+                    loveTally += 1;
+                    break;
 
-            case Feel.SADNESS:
-                sadnessTally += 1;
-                break;
+                case Feel.SADNESS:
+                    sadnessTally += 1;
+                    break;
 
-            case Feel.SURPRISE:
-                surpriseTally += 1;
-                break;
+                case Feel.SURPRISE:
+                    surpriseTally += 1;
+                    break;
 
-            default:
-                Log.e(TAG, "Unsupported feeling attempted to be tallied: " + feeling);
-                break;
+                default:
+                    Log.e(TAG, "Unsupported feeling attempted to be tallied: " + feeling);
+                    break;
+            }
         }
-        return super.add(feel);
+        return offerResult;
     }
 
     public Integer getLoveTally() {
