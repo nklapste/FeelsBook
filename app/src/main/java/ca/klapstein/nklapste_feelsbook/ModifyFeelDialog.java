2ccec4f3267a1c412ed9e9b5627bfb857d4efc10
Nathan Klapstein <nklapste@ualberta.ca>
2018-10-05 00:38:01 -0600

    moving to AppCompat

--- app/src/main/java/ca/klapstein/nklapste_feelsbook/ModifyFeelDialog.java
+++ app/src/main/java/ca/klapstein/nklapste_feelsbook/ModifyFeelDialog.java
@@ -1,6 +1,5 @@
 package ca.klapstein.nklapste_feelsbook;
 
-import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -9,15 +8,11 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-
+import android.widget.*;
 import com.ikovac.timepickerwithseconds.MyTimePickerDialog;
 
 import java.text.ParseException;
